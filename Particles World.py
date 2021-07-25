bd407e23566581c81574deeb17787606a5a9497b
SGNull88 <60150561+SGNull88@users.noreply.github.com>
2021-07-25 01:27:15 -0400

    Added keyboard listener, fixed some major bugs.

--- Particles World.py
+++ Particles World.py
@@ -1,22 +1,35 @@
 from graphics import Point, Circle, GraphWin
+import keyboard
 from time import sleep
 from random import random
 
+# TODO: Figure out how to properly add a version number to the document.
+# Screen settings
+SCREEN_NAME = "Particle Simulation 0.0.1"
 SCREEN_HEIGHT = 800
 SCREEN_WIDTH = 1200
-SIM_SPEED = 1
-PARTICLE_COUNT = 3
-PARTICLE_SPEED = 2
-PARTICLE_SIZE = 5
+SCREEN_COLOR = "black"
 
-Universe = list
+# Universe settings
+SIM_SPEED = 3
+
+# Particle specific settings
+PARTICLE_COUNT = 3
+PARTICLE_SPEED = 5
+PARTICLE_SIZE = 5
+PARTICLE_COLOR = "white"
+
+# Important variables which should be global in scope.
+Universe = []  # Global because it contains every object in the universe, which needs to be acted on in nearly every function
+Running = True   # Global because it controls main, but also needs to be altered by key presses.
 
 class Particle():
     def __init__(self, xPos = 0, yPos = 0) -> None:
         self._pos = [xPos, yPos]
         self._prev = [0,0]
         self._vel = [0,0]
-        self._graphic = Circle(Point(self._pos[0], self._pos[1]), PARTICLE_SIZE).setFill("black")
+        self._graphic = Circle(Point(self._pos[0], self._pos[1]), PARTICLE_SIZE)
+        self._graphic.setFill("black")
         self._drawn = False
 
     def set_velocity(self, deltaX, deltaY):
@@ -27,13 +40,14 @@ class Particle():
         self._pos[1] = (self._pos[1] + self._vel[1]) % SCREEN_HEIGHT
 
     def draw(self, window):
-        # This one-time if-else statement costs us about 0.04 microseconds on average, probably less given processor optimizations (prediction algorithms).
+        # I was debating a lot on whether this single-use if-else statement is a good idea. It makes drawing the particles easier, but I was concerned about speed.
+        # It costs us about 0.04 microseconds on average, probably less given predictive branching in the processor (just a guess, not sure how python->machine-code works).
         # Source: https://stackoverflow.com/questions/2522005/cost-of-exception-handlers-in-python
         if not self._drawn:
             self._graphic.draw(window)
             self._drawn = True
         else:
-            self._graphic.move(self._xPos - self._xPrev, self._yPos - self._yPrev)
+            self._graphic.move(self._pos[0] - self._prev[0], self._pos[1] - self._prev[1])
 
         self._prev[0] = self._pos[0]
         self._prev[1] = self._pos[1]
@@ -42,8 +56,8 @@ class Particle():
 def init_universe():
     for i in range(PARTICLE_COUNT):
         newParticle = Particle(random()*SCREEN_WIDTH, random()*SCREEN_HEIGHT)
-        newParticle.set_velocity(random()*PARTICLE_SPEED, random()*PARTICLE_SPEED)
-        Universe.append()
+        newParticle.set_velocity(rand_between(-PARTICLE_SPEED, PARTICLE_SPEED), rand_between(-PARTICLE_SPEED, PARTICLE_SPEED))
+        Universe.append(newParticle)
 
 def phys_step():
     for particle in Universe:
@@ -53,18 +67,32 @@ def draw_universe(window):
     for particle in Universe:
         particle.draw(window)
 
+def rand_between(start, end):
+    output = random()*(end - start)
+    output += start
+    return output
+
 
 def main():
     print("Generating universe...")
     init_universe()
-    window = GraphWin(SCREEN_WIDTH, SCREEN_HEIGHT)
+    window = GraphWin(SCREEN_NAME, SCREEN_WIDTH, SCREEN_HEIGHT)
 
-    print("Starting simulation")
-    running = True
-    while running:
+    print("Starting simulation. Press a to quit")
+    global Running
+    while Running:
         sleep(1/(10*SIM_SPEED))
         phys_step()
         draw_universe(window)
 
+    window.close()
 
+# THIS WORKS!!! AND IT ISN'T A COPYPASTE FROM STACKOVERFLOW!!
+def stop_main(event):
+    global Running
+    Running = False
+keyboard.on_press_key("a", stop_main)
+
+# Starting/Ending code
 main()
+keyboard.unhook_all()
